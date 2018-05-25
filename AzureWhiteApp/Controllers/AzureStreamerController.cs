930352462bbd62fcc8615ba18a788cde55ff0a0c
laurence.habdas <laurence.habdas@gmail.com>
2018-05-25 15:59:30 +0200

    first commit

---
+++ AzureWhiteApp/Controllers/AzureStreamerController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureWhiteApp;
+using AzureWhiteApp.Hub;
+using HdrHistogram;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace WhiteApp.Controllers
+{
+    public class AzureStreamerController : Controller
+    {
+        private readonly IHubContext<Streamer> context;
+
+        public AzureStreamerController(IHubContext<Streamer> context)
+        {
+            this.context = context;
+        }
+
+        [Route("/api/signalr-streamer/")]
+        [HttpGet]
+        public void SignalRStreamer()
+        {
+
+            var histogram = new LongHistogram(TimeStamp.Hours(1), 3);
+
+            var writer = new StringWriter();
+
+            Task.Run(async () =>
+            {
+                for (var i = 1; i < 1000;i++)
+                {
+
+                    long startTimestamp = Stopwatch.GetTimestamp();
+
+                    var currency = new Currency()
+                    {
+                        Id = 1,
+                        CurrencyType = "EUR/USD",
+                        Price = i,
+                        Timestamp = startTimestamp.ToString()
+                    };
+
+                    var cur = JsonConvert.SerializeObject(currency);
+                    await context.Clients.Group("myGroup").SendAsync("broadcastMessage", "currency", cur).ConfigureAwait(false);
+
+                    long elapsed = Stopwatch.GetTimestamp() - startTimestamp;
+                    histogram.RecordValue(elapsed);
+
+                    await Task.Delay(50).ConfigureAwait(false);
+                }
+                var scalingRatio = OutputScalingFactor.TimeStampToMilliseconds;
+                histogram.OutputPercentileDistribution(
+                    writer,
+                    outputValueUnitScalingRatio: scalingRatio);
+                System.IO.File.WriteAllText(@"d:\cloud\signalr.txt", writer.ToString());
+
+            });
+        }
+
+    }
+
+
+}
---
+++ AzureWhiteApp/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureWhiteApp.Hub;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AzureWhiteApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSignalR()
+                .AddAzureSignalR("");
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+            app.UseFileServer();
+            app.UseAzureSignalR(routes =>
+            {
+                routes.MapHub<Streamer>("/streamer");
+            });
+
+
+
+        }
+    }
+}
